ac1ac5bb15ba436f6e4ab37946fad027c09f7746
HY-ZhengWei <hy.zhengwei@qq.com>
2017-12-30 10:24:16 +0800

    3.1.3
    Signed-off-by: HY-ZhengWei <hy.zhengwei@qq.com>

---
+++ Maven.Deploy.Win.Report.bat
@@ -0,0 +1,1 @@
+mvn deploy:deploy-file -Dfile=hy.common.report.jar -DpomFile=./src/META-INF/maven/org/hy/common/report/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:9015/nexus/content/repositories/thirdparty
